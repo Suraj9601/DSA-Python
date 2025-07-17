f8ef043dd8007da748adb0a27f2be880c73446f1
Suraj Aglave <aglavesuraj96@gmail.com>
2025-07-17 20:15:55 +0530

    Update DeleteArrayelement.py

--- DeleteArrayelement.py
+++ DeleteArrayelement.py
@@ -2,7 +2,6 @@ arr = [1, 2, 3, 4, 5]
 print("Original array:", arr)
 
 element = int(input("Enter element you want to delete: "))
-
 found = False
 
 for i in range(len(arr)):
@@ -10,7 +9,6 @@ for i in range(len(arr)):
         arr.pop(i)
         found = True
         break 
-
 if found:
     print("Updated array:", arr)
 else:
