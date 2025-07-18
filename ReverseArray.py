05455f56e1f9c15261dd8198ad1056409640cdd8
Suraj Aglave <aglavesuraj96@gmail.com>
2025-07-18 16:53:12 +0530

    Update ReverseArray.py

--- ReverseArray.py
+++ ReverseArray.py
@@ -1,3 +1,11 @@
-arr = list(map(int , input("Enter ele : ")))
-print(arr)
+arr = [1, 2, 4, 5,  6, 7, 8, 9, 10]
 
+# arr.reverse()
+# print(arr )
+
+for i in range(len(arr)):
+    arr[i] = arr[len(arr)-1]
+    
+
+
+print(arr)
