bca6f0dbc437e733bb0fe1451e36161cab4daa16
Suraj Aglave <aglavesuraj96@gmail.com>
2025-07-18 18:21:10 +0530

    Update ReverseArray.py

--- ReverseArray.py
+++ ReverseArray.py
@@ -1,11 +1,14 @@
 arr = [1, 2, 4, 5,  6, 7, 8, 9, 10]
+print(arr)
 
-# arr.reverse()
-# print(arr )
+def reverse(arr):
+    left = 0
+    right = len(arr) - 1
 
-for i in range(len(arr)):
-    arr[i] = arr[len(arr)-1]
-    
+    while left < right:
+        arr[right],arr[left] = arr[left],arr[right]
+        left += 1
+        right -= 1
 
-
+reverse(arr)
 print(arr)
