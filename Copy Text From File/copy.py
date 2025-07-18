d4d01639453cbfdb83e21d07369dc2e2cfd75664
Suraj Aglave <aglavesuraj96@gmail.com>
2025-07-18 13:39:24 +0530

    Update copy.py

--- Copy Text From File/copy.py
+++ Copy Text From File/copy.py
@@ -1,10 +1,14 @@
 
 with open("a.txt", 'r') as main_file:
-    content = main_file.read()
+    content1 = main_file.read()
     
-
-with open("b.txt", 'w') as copy_file:
-    copy_file.write(content)
-
+with open("b.txt", 'r') as check_file:
+    content2 = check_file.read()
     
-print("Text copy successful.")
+    if (content2 != content1):
+        with open("b.txt", 'w') as copy_file:
+            copy_file.write(content1)
+            print("Text copy successful.")
+    else:
+        print("Text already copyed")
+
