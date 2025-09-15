51085be63e74cecf843a3b0dbe89dc126e1c039f
Suraj Aglave <aglavesuraj96@gmail.com>
2025-09-15 19:13:47 +0530

    Delete InsertArrayElement.py

--- InsertArrayElement.py
+++
@@ -1,9 +0,0 @@
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print("Original Array :", arr)
-
-element = int(input("Enter the element do you want insert : "))
-position =   int(input("Enter position of element you want add : ")) 
-position -= 1
-
-arr.insert(position,element)
-print(arr)
